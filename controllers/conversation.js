2e6abc67462cd17658402ba2ac9469e63a5714bc
voxtool <voxtool@gmail.com>
2021-05-19 23:44:24 +0300

    minor change

--- controllers/conversation.js
+++ controllers/conversation.js
@@ -3,7 +3,7 @@ const { Conversation } = require('../models');
 async function getConversations(req, res, next) {
     const { _id: userId } = req.user;
     try {
-        const conversations = await Conversation.find({ recipients: { $in: userId } }).sort({ created_at: -1 });
+        const conversations = await Conversation.find({ recipients: { $in: userId } });
         res.status(200).json(conversations);
     } catch (err) {
         next(err);
